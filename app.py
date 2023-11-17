f88f790ef8eaf8258ca1fb68526d95ee99deed10
noahs-ark-syndrome <145132301+noahs-ark-syndrome@users.noreply.github.com>
2023-11-17 14:55:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,9 @@ import streamlit as st
 import requests
 
 
-def main():
+def request_dog():
     api_url = "https://dog.ceo/api/breeds/image/random"
     r = requests.get(api_url)
-    data = r.json()
-
-    image_url = data["message"]
 
 # ユーザーインターフェイスの構築
 st.title("ランダムわんこ")
@@ -15,4 +12,6 @@ st.image("https://th.bing.com/th/id/OIG.Byw03p.SxchBtY6HtdH1?pid=ImgGn")
 st.write("いろんなわんこをお見せしましょう")
 if st.button("わん！"):
     main()
-    st.image(data["message"])
+    data = r.json()
+    image_url = data["message"]
+    st.image(image_url)
