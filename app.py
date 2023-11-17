c6d8de103334e552c6aae7c9681300e2671bce5b
noahs-ark-syndrome <145132301+noahs-ark-syndrome@users.noreply.github.com>
2023-11-17 14:11:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ st.title("ランダムわんこ")
 st.image("https://th.bing.com/th/id/OIG.Byw03p.SxchBtY6HtdH1?pid=ImgGn")
 st.write("いろんなわんこをお見せしましょう")
 if st.button("わん！"):
-    st.image(str(image_url))
+    st.image(image_url)
 
 if __name__ == "__main__":
     main()
