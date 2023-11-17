5a2e60b5b372e472baf834891c469fbd9c81f0f1
noahs-ark-syndrome <145132301+noahs-ark-syndrome@users.noreply.github.com>
2023-11-17 14:50:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,6 @@
 import streamlit as st
 import requests
 
-def download_image(url, file_path):
-    """
-    url: ダウンロードの元になるURL
-    file_path: 保存先のファイルパス
-    """
-    r = requests.get(url, stream = True)
-
-    if r.status_code == 200:
-        with open(file_path, "wb") as f:
-            f.write(r.content)
 
 def main():
     api_url = "https://dog.ceo/api/breeds/image/random"
@@ -25,4 +15,4 @@ st.image("https://th.bing.com/th/id/OIG.Byw03p.SxchBtY6HtdH1?pid=ImgGn")
 st.write("いろんなわんこをお見せしましょう")
 if st.button("わん！"):
     main()
-    st.image(str(image_url))
+    st.image("https://images.dog.ceo/breeds/wolfhound-irish/n02090721_4006.jpg")
